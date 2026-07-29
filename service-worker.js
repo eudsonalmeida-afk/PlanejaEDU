d096c4cd8545f5e66cddfd133284d38ee855bafc
eudsonalmeida-afk <eudson.almeida@gmail.com>
2026-07-29 19:47:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,55 @@
-const CACHE_NAME = 'ag3-planos-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+const CACHE_NAME = "planejaedu-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-// Instalação do Service Worker e salvamento dos arquivos essenciais em cache
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
   );
+
+  self.skipWaiting();
 });
 
-// Ativação e limpeza de versões antigas de cache
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
   );
+
+  self.clients.claim();
 });
 
-// Interceptação de requisições de rede para garantir funcionamento offline
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then(response => {
-        if (response) {
-          return response; // Retorna a versão em cache se disponível
-        }
-        return fetch(event.request); // Caso contrário, busca na internet
+        const responseCopy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseCopy);
+        });
+
+        return response;
       })
+      .catch(() =>
+        caches.match(event.request).then(cachedResponse => {
+          return cachedResponse || caches.match("./index.html");
+        })
+      )
   );
 });
